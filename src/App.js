540cce7a0b7c9f2d0a24a0fdd01686e184cef02b
matin808 <matinkadri07@gmail.com>
2020-10-04 12:14:51 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World, Lets Build Instagram CLone</h1>
+    </div>
+  );
+}
+
+export default App;
